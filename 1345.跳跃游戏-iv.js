14ff77aefa7990b7e638536f0831c2646fb4ee12
“fanhuanxi” <fanhuanxi@meituan.com>
2022-09-10 09:51:37 +0800

    2022.09.10

---
+++ 1345.跳跃游戏-iv.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1345 lang=javascript
+ *
+ * [1345] 跳跃游戏 IV
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+// 转化成图中的最短路径
+var minJumps = function(arr) {
+  
+};
+// @lc code=end
+
