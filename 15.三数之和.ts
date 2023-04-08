618acbd617af2c65e2444b17ea2cdd658a8167c8
huanxi <2290991129@qq.com>
2023-04-08 11:34:49 +0800

    三数之和优化

--- 15.三数之和.ts
+++ 15.三数之和.ts
@@ -13,8 +13,12 @@ function threeSum(nums: number[]): number[][] {
   for (let i = 0; i < nums.length - 2; i++) {
     // 避免重复
     if (i > 0 && nums[i] === nums[i - 1]) continue
-    let j = i + 1, k = nums.length - 1
-    while(j < k) {
+    // 提前退出
+    if (nums[i] + nums[i + 1] + nums[i + 2] > 0) break
+    if (nums[i] + nums[nums.length - 2] + nums[nums.length - 1] < 0) continue
+    let j = i + 1,
+      k = nums.length - 1
+    while (j < k) {
       const sum = nums[i] + nums[j] + nums[k]
       if (sum === 0) {
         res.push([nums[i], nums[j], nums[k]])
@@ -31,6 +35,6 @@ function threeSum(nums: number[]): number[][] {
   }
   return res
 }
-let res = threeSum([-1,0,1,2,-1,-4,-2,-3,3,0,4])
+let res = threeSum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4])
 console.log(res)
 // @lc code=end
