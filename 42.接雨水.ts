3380c214b5ca7b9289eac065bbcf126f422f8b83
huanxi <2290991129@qq.com>
2023-04-18 23:46:18 +0800

    接雨水优化

--- 42.接雨水.ts
+++ 42.接雨水.ts
@@ -5,32 +5,19 @@
  */
 
 // @lc code=start
-function getPreMax(nums: number[]): number[] {
-  let pre_max: number[] = [],
-    max = -Infinity
-  for (let i = 0; i < nums.length; i++) {
-    max = Math.max(max, nums[i])
-    pre_max.push(max)
-  }
-  return pre_max
-}
-
-function getSufMax(nums: number[]): number[] {
-  let suf_max: number[] = [],
-    max = -Infinity
-  for (let i = nums.length - 1; i >= 0; i--) {
-    max = Math.max(max, nums[i])
-    suf_max.unshift(max)
-  }
-  return suf_max
-}
-
 function trap(height: number[]): number {
-  let res: number = 0,
-    pre_max: number[] = getPreMax(height),
-    suf_max: number[] = getSufMax(height)
-  for (let i = 0; i < height.length; i++) {
-    res += Math.max(0, Math.min(pre_max[i], suf_max[i]) - height[i])
+  let res: number = 0, pre_max = -Infinity, suf_max = -Infinity
+  let left = 0, right = height.length - 1
+  while (left <= right) {
+    if (height[left] > height[right]) {
+      suf_max = Math.max(suf_max, height[right])
+      res += Math.max(0, suf_max - height[right])
+      right--
+    } else {
+      pre_max = Math.max(pre_max, height[left])
+      res += Math.max(0, pre_max - height[left])
+      left++
+    }
   }
   return res
 }
