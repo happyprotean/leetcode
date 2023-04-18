4fb45bbc6d6a9d5dea337645b8ef6148637583b6
huanxi <2290991129@qq.com>
2023-04-18 22:33:01 +0800

    接雨水计算前缀优化

--- 42.接雨水.ts
+++ 42.接雨水.ts
@@ -6,37 +6,32 @@
 
 // @lc code=start
 function getPreMax(nums: number[]): number[] {
-  let pre_max: number[] = [], max = -Infinity
+  let pre_max: number[] = [],
+    max = -Infinity
   for (let i = 0; i < nums.length; i++) {
-    if (i === 0) {
-      pre_max.push(max)
-    } else {
-      max = Math.max(max, nums[i - 1])
-      pre_max.push(max)
-    }
+    max = Math.max(max, nums[i])
+    pre_max.push(max)
   }
   return pre_max
 }
 
 function getSufMax(nums: number[]): number[] {
-  let suf_max: number[] = [], max = -Infinity
+  let suf_max: number[] = [],
+    max = -Infinity
   for (let i = nums.length - 1; i >= 0; i--) {
-    if (i === nums.length - 1) {
-      suf_max.unshift(max)
-    } else {
-      max = Math.max(max, nums[i + 1])
-      suf_max.unshift(max)
-    }
+    max = Math.max(max, nums[i])
+    suf_max.unshift(max)
   }
   return suf_max
 }
 
 function trap(height: number[]): number {
-  let res: number = 0, pre_max: number[] = getPreMax(height), suf_max: number[] = getSufMax(height);
+  let res: number = 0,
+    pre_max: number[] = getPreMax(height),
+    suf_max: number[] = getSufMax(height)
   for (let i = 0; i < height.length; i++) {
     res += Math.max(0, Math.min(pre_max[i], suf_max[i]) - height[i])
   }
   return res
-};
+}
 // @lc code=end
-
