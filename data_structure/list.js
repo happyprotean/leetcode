4418db626145ad2cd07d0cdd9ad7f6e130e0341f
huanxi <2290991129@qq.com>
2023-04-22 17:21:33 +0800

    增加链表的定义，便于测试

---
+++ data_structure/list.js
@@ -0,0 +1,27 @@
+
+class ListNode {
+  constructor(val, next) {
+    this.val = val
+    this.next = next || null
+  }
+}
+
+export class List {
+  constructor(valList) {
+    this.head = this.createList(valList)
+  }
+
+  createList(arr) {
+    if (arr.length === 0) return null
+    let dummy = new ListNode(-1), cur = dummy
+    for (let i = 0; i < arr.length; i++) {
+      cur.next = new ListNode(arr[i])
+      cur = cur.next 
+    }
+    return dummy.next
+  }
+   
+  getHead() {
+    return this.head
+  }
+}
