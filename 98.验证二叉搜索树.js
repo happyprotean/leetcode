c5b1abe6b4364c0c7e8918b492f44262109c7b71
huanxi <2290991129@qq.com>
2023-05-07 20:49:08 +0800

    二叉搜索树

--- 98.验证二叉搜索树.js
+++ 98.验证二叉搜索树.js
@@ -18,11 +18,11 @@
  * @return {boolean}
  */
 // 前序遍历，首先判断根节点是否满足区间条件，然后依次判断左右子节点
-// var isValidBST = function(root, min = -Infinity, max = Infinity) {
-//   if (!root) return true
-//   const val = root.val
-//   return val > min && val < max && isValidBST(root.left, min, val) && isValidBST(root.right, val, max)
-// };
+var isValidBST = function(root, min = -Infinity, max = Infinity) {
+  if (!root) return true
+  const val = root.val
+  return val > min && val < max && isValidBST(root.left, min, val) && isValidBST(root.right, val, max)
+};
 
 // 中序遍历，对应的节点是一个升序数组
 // 将节点值存入数组再依次判断，占用O(n)空间
@@ -41,6 +41,21 @@ var isValidBST = function(root) {
   }
   help(root)
   return pre !== Infinity
+};
+
+// 后序遍历，将左右子树的取值范围向上传递，判断根节点是否在合理的范围内
+var isValidBST = function(root) {
+  // help函数返回当前数的最小值和最大值
+  const help = node => {
+    // [Infinity, -Infinity]确保node.val <= l_max || node.val >= r_min不成立
+    if (!node) return [Infinity, -Infinity]
+    const [l_min, l_max] = help(node.left)
+    const [r_min, r_max] = help(node.right)
+    //[-Infinity, Infinity] 表示非二叉搜索树，且可以保证将此结果一直上传，直到函数入口
+    if (node.val <= l_max || node.val >= r_min) return [-Infinity, Infinity]
+    return [Math.min(l_min, node.val), Math.max(r_max, node.val)]
+  }
+  return help(root)[0] === -Infinity ? false : true
 };
 // @lc code=end
 
