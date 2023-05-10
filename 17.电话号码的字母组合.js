02ef1795c41dfb5d4c86df135211439b0c23beb6
huanxi <2290991129@qq.com>
2023-05-10 22:40:09 +0800

    电话号码的字母组合

--- 17.电话号码的字母组合.js
+++ 17.电话号码的字母组合.js
@@ -12,18 +12,21 @@
 var letterCombinations = function(digits) {
   if (digits.length === 0) return []
   const num2char = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+  // 最终结果
   let res = [], len = digits.length
+  // 某个回溯分支的结果
   let cur = new Array(digits.length).fill('')
   // 表示当前遍历到digits的第i位
   const dfs = i => {
+    // 递归结束，将回溯分支结果记录
     if (i === len) {
       res.push(cur.join(''))
       return
     }
     const chars = num2char[Number(digits[i])]
     // 第i位依次枚举
-    for (let k = 0; k < chars.length; k++) {
-      cur[i] = chars[k]
+    for (const char of chars) {
+      cur[i] = char
       // 遍历digits的第i+1位
       dfs(i+1)
     }
