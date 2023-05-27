ca3007f1973d595ad84228ae14b6d2f1dc1017d1
“fanhuanxi” <fanhuanxi@meituan.com>
2023-05-27 18:52:10 +0800

    零钱兑换

--- 322.零钱兑换.js
+++ 322.零钱兑换.js
@@ -13,18 +13,17 @@
 // 将物品的价值定义为1的话，最少硬币数可以转换为：恰好amount时，获取的最小价值
 var coinChange = function (coins, amount) {
   const n = coins.length
-  const memo = Array.from({ length: n + 1 }, () => Array(amount + 1).fill(Infinity))
-  memo[0][0] = 0
+  const memo = Array(amount + 1).fill(Infinity)
+  memo[0] = 0
   for (let i = 0; i < coins.length; i++) {
+    // 可以正序遍历
     for (let k = 0; k < amount + 1; k++) {
-      if (coins[i] > k) {
-        memo[i + 1][k] = memo[i][k]
-      } else {
-        memo[i + 1][k] = Math.min(memo[i][k], memo[i + 1][k - coins[i]] + 1)
+      if (coins[i] <= k) {
+        memo[k] = Math.min(memo[k], memo[k - coins[i]] + 1)
       }
     }
   }
-  const res = memo[n][amount]
+  const res = memo[amount]
   return res < Infinity ? res : -1
 }
 // @lc code=end
